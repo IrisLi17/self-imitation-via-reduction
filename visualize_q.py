1b3706565f4e3fec99c384e38f2d0211b66e84b7
Yunfei Li <l-yf16@mails.tsinghua.edu.cn>
2019-10-31 17:15:46 +0800

    Visualizing value fn wrt obstacle and end effector position

--- visualize_q.py
+++ visualize_q.py
@@ -9,11 +9,16 @@ from stable_baselines.her import HER
 
 env_name = 'FetchPushWallObstacle-v1'
 kwargs = dict(random_box=False, heavy_obstacle=True)
-load_path = 'logs/FetchPushWallObstacle-v1_heavy_random/her_sac2/' # U shape valley
+# load_path = 'logs/FetchPushWallObstacle-v1_heavy_random/her_sac2/' # U shape valley
 load_path = 'logs/FetchPushWallObstacle-v1_heavy_determine/her_sac2/' # 2 peak regions
 load_path = 'logs/FetchPushWallObstacle-v1_heavy_purerandom/her_sac/' # V chair, the minimum point is *not* expected
+load_path = 'logs/FetchPushWallObstacle-v1_heavy_purerandom_hack3/her_sac/'
 # load_path = 'logs/FetchPushWallObstacle-v1_heavy_mix0.7/her_sac' # ?
-# load_path = 'logs/FetchPushWallObstacle-v1_heavy_random_hack/her_sac'
+# load_path = 'logs/FetchPushWallObstacle-v1_heavy_random_hack/her_sac' # a valley
+# load_path = 'logs/FetchPushWallObstacle-v1_heavy_mix0.7_hack/her_sac' # deep V in x=1.3
+# load_path = 'logs/FetchPushWallObstacle-v1_heavy_random_hack2/her_sac'
+load_path = 'logs/FetchPushWallObstacle-v1_heavy_random_hack3/her_sac'
+# load_path = 'logs/FetchPushWallObstacle-v1_heavy_random_hack4/her_sac'
 # kwargs = dict(random_box=False, heavy_obstacle=False)
 # load_path = 'logs/FetchPushWallObstacle-v1_light_random/her_sac2/' # flat in ypos
 # load_path = 'logs/FetchPushWallObstacle-v1_light_determine/her_sac2/' # W shape, rough
@@ -21,7 +26,7 @@ load_path = 'logs/FetchPushWallObstacle-v1_heavy_purerandom/her_sac/' # V chair,
 # load_path = 'logs/FetchPushWallObstacle-v1_light_random_hack/her_sac/' # flat
 gym.register(env_name, entry_point=ENTRY_POINT[env_name], max_episode_steps=50, kwargs=kwargs)
 env = gym.make(env_name)
-model = HER.load(os.path.join(load_path, 'model_40.zip'), env=env)
+model = HER.load(os.path.join(load_path, 'model_27.zip'), env=env)
 env = model.env
 
 if os.path.exists('batch_obs.npy'):
@@ -43,7 +48,7 @@ if os.path.exists('batch_obs.npy'):
     grid_shape = obstacle_xpos.shape
     _obstacle_xpos = obstacle_xpos.reshape(-1, 1)
     _obstacle_ypos = obstacle_ypos.reshape(-1, 1)
-    batch_obs = np.tile(batch_obs[13], (_obstacle_xpos.shape[0], 1))
+    batch_obs = np.tile(batch_obs[8], (_obstacle_xpos.shape[0], 1))
     # batch_obs = np.tile(batch_obs[8], (_obstacle_xpos.shape[0], 1))
     batch_obs[:, 6] = _obstacle_xpos[:, 0]
     batch_obs[:, 7] = _obstacle_ypos[:, 0]
@@ -51,8 +56,8 @@ if os.path.exists('batch_obs.npy'):
     batch_obs[:, 13] = batch_obs[:, 7] - batch_obs[:, 1]
     batch_obs[:, -2] = 0.75
     # batch_actions = np.tile(np.array([-1.0, 0.0, 0.0, 0.0]), (_obstacle_xpos.shape[0], 1))
-    batch_actions = np.tile(batch_actions[13], (_obstacle_xpos.shape[0], 1))
-    imgs = np.tile(imgs[13], (_obstacle_xpos.shape[0], 1, 1, 1))
+    batch_actions = np.tile(batch_actions[8], (_obstacle_xpos.shape[0], 1))
+    imgs = np.tile(imgs[8], (_obstacle_xpos.shape[0], 1, 1, 1))
     # imgs = np.tile(imgs[8], (_obstacle_xpos.shape[0], 1, 1, 1))
     # batch_obs = np.asarray(_batch_obs)
     # batch_actions = np.asarray(_batch_actions)
@@ -79,12 +84,11 @@ else:
 sac_model = model.model
 feed_dict = {
             sac_model.observations_ph: batch_obs,
-            sac_model.actions_ph: batch_actions,
 }
 # TODO: make animation
 fig = plt.figure(figsize=(6, 6))
 ax1 = fig.add_subplot(111, projection='3d')
-for f_idx in range(41):
+for f_idx in range(60):
     model.model.load_parameters(os.path.join(load_path, 'model_' + str(f_idx) + '.zip'))
     values = sac_model.sess.run(sac_model.step_ops[6], feed_dict)
     grid_values = np.reshape(values, grid_shape)
@@ -103,53 +107,3 @@ for f_idx in range(41):
     plt.pause(0.1)
 plt.show()
 exit()
-
-values = sac_model.sess.run(sac_model.step_ops[6], feed_dict) # value_fn
-grid_values = np.reshape(values, grid_shape)
-print(batch_obs.shape, batch_actions.shape, values.shape)
-# TODO: print idx when value reaches minimum along x=1.3 and y=0.7
-
-# print(grid_values)
-# print(obstacle_xpos)
-# print(obstacle_ypos)
-box_pos = batch_obs[:, 40:43]
-obstacle_pos = batch_obs[:, 6:9]
-min_value = np.min(values)
-max_value = np.max(values)
-
-fig = plt.figure(figsize=(12, 6))
-ax1 = fig.add_subplot(122, projection='3d')
-# ax2 = fig.add_subplot(122, projection='3d')
-ax2 = fig.add_subplot(121)
-surf = ax1.plot_surface(obstacle_xpos, obstacle_ypos, grid_values, cmap=cm.coolwarm)
-ax1.set_xlim(0.9, 1.7)
-ax1.set_ylim(0.3, 1.1)
-ax1.set_xlabel('obstacle xpos')
-ax1.set_ylabel('obstacle ypos')
-ax1.set_zlim(0, 8)
-ax1.set_zlabel('value fn')
-ax1.view_init(elev=60, azim=135)
-fig.colorbar(surf)
-ax2.imshow(imgs[0])
-plt.show()
-exit()
-for f_idx in range(box_pos.shape[0]):
-    ax2.cla()
-    ax3.cla()
-    ax1.scatter(box_pos[f_idx, 0], box_pos[f_idx, 1], values[f_idx, 0], c='k')
-    # ax1.set_title('target ' + str(batch_obs[0, -3:]))
-    ax1.set_xlim(0.9, 1.7)
-    ax1.set_ylim(0.3, 1.1)
-    ax1.set_xlabel('box xpos')
-    ax1.set_ylabel('box ypos')
-    ax1.set_zlim(min_value, max_value)
-    # ax2.scatter(obstacle_pos[f_idx, 0], obstacle_pos[f_idx, 1], values[f_idx, 0], c='k')
-    ax2.plot_surface(obstacle_xpos, obstacle_ypos, grid_values, cmap=cm.coolwarm)
-    ax2.set_xlim(0.9, 1.7)
-    ax2.set_ylim(0.3, 1.1)
-    ax2.set_xlabel('obstacle xpos')
-    ax2.set_ylabel('obstacle ypos')
-    ax2.set_zlim(min_value, max_value)
-    ax3.imshow(imgs[f_idx])
-    plt.pause(0.1)
-plt.show()
