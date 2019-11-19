a1d87a6d0d116a49b3df85c1177031b32d1c6755
Yunfei Li <l-yf16@mails.tsinghua.edu.cn>
2019-11-19 18:37:05 +0800

    Hack to switch goal

--- visualize_ensemble.py
+++ visualize_ensemble.py
@@ -3,14 +3,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 from test_ensemble import make_env
 from stable_baselines import HER
+from baselines import HER_HACK
 from stable_baselines.her.utils import KEY_ORDER
 
 
 def reset_goal(env):
     obs = env.reset()
     env.goal[0] = 1.2
-    env.goal[1] = obs['achieved_goal'][1]
+    env.goal[1] = obs['observation'][4]
+    # env.goal[1] = 0.75
+    if len(env.goal) == 5:
+        env.goal[3:] = np.array([1., 0])
     obs['desired_goal'] = env.goal
+    obs['achieved_goal'][0:3] = obs['observation'][3:6]
+    if len(env.goal) == 5:
+        obs['achieved_goal'][3:] = np.array([1., 0])
     print(obs)
     return obs
 
@@ -49,30 +56,74 @@ def generate_trajectory(env, obs, model, free=False, greedy=True):
         obs, _, _, _ = env.step(action)
         img = env.render(mode='rgb_array')
         imgs.append(img)
-        # plt.imshow(img)
-        # plt.pause(0.1)
+        plt.imshow(img)
+        plt.pause(0.1)
     batch_obs = np.asarray(batch_obs)
     batch_imgs = np.asarray(imgs)
     return batch_obs, batch_imgs
 
 
+# HACK goal
+def hack_trajectory(env, obs, model, free=True, greedy=False):
+    batch_obs = []
+    imgs = []
+    flag = True
+    box_goal = obs['desired_goal'].copy()
+    print('box goal', box_goal)
+    for i in range(50):
+        if flag and np.argmax(obs['desired_goal'][3:]) == 0:
+            # Switch the target to move obstacle
+            print(str(i), 'switch target to obstacle')
+            env.goal[3:] = np.array([0, 1.])
+            env.goal[0:3] = obs['observation'][6:9]
+            env.goal[1] -= 0.12
+            obs['desired_goal'] = env.goal.copy()
+            obs['achieved_goal'][0:3] = obs['observation'][6:9]
+            obs['achieved_goal'][3:] = np.array([0, 1.])
+            flag = False
+        elif np.argmax(obs['desired_goal'][3:]) == 1 and np.linalg.norm(obs['observation'][6:9] - obs['desired_goal'][0:3]) < env.distance_threshold:
+            # Switch the target to move box
+            print(str(i), 'switch target to box', box_goal)
+            env.goal[:] = box_goal
+            obs['desired_goal'] = box_goal
+            obs['achieved_goal'][0:3] = obs['observation'][3:6]
+            obs['achieved_goal'][3:] = np.array([1., 0])
+        action, _ = model.predict(obs)
+        batch_obs.append(np.concatenate([obs[key] for key in KEY_ORDER]))
+        obs, _, _, _ = env.step(action)
+        img = env.render(mode='rgb_array')
+        imgs.append(img)
+        plt.imshow(img)
+        plt.title('goal ' + str(obs['desired_goal']))
+        plt.pause(0.1)
+    batch_obs = np.asarray(batch_obs)
+    batch_imgs = np.asarray(imgs)
+    return batch_obs, batch_imgs
+
+
+# python visualize_ensemble.py FetchPushWallObstacle-v4 logs/FetchPushWallObstacle-v4_heavy_purerandom/her_sac/custom/model_70.zip 1 0
 if __name__ == '__main__':
-    if len(sys.argv) < 3:
-        print('Usage: python visualize_ensemble.py [load_path] [greedy 0|1]')
+    if len(sys.argv) < 5:
+        print('Usage: python visualize_ensemble.py [env_name] [load_path] [free 0|1] [greedy 0|1]')
         exit()
-    env_name = 'FetchPushWallObstacle-v1'
-    load_path = sys.argv[1]
-    greedy = int(sys.argv[2]) == 1
+    env_name = sys.argv[1]
+    her_class = HER_HACK if env_name == 'FetchPushWallObstacle-v4' else HER
+    load_path = sys.argv[2]
+    free = int(sys.argv[3]) == 1
+    greedy = int(sys.argv[4]) == 1
     env_kwargs = dict(random_box=False,
                       heavy_obstacle=True,
                       random_ratio=1.0,
                       )
     env = make_env(env_name, **env_kwargs)
-    model = HER.load(load_path, env=env)
+    model = her_class.load(load_path, env=env)
     obs = reset_goal(env)
     print(env.goal, obs['achieved_goal'], obs['desired_goal'])
-    batch_obs, batch_imgs = generate_trajectory(env, obs, model, free=False, greedy=greedy)
-
+    batch_obs, batch_imgs = generate_trajectory(env, obs, model, free=free, greedy=greedy)
+    # HACK goal
+    # batch_obs, batch_imgs = hack_trajectory(env, obs, model, free=free, greedy=greedy)
+    # imageio.mimsave('free_universe_hackgoal.gif', batch_imgs)
+    # exit()
     sac_model = model.model
     value_ensemble_op = sac_model.step_ops[-1]
     feed_dict = {
