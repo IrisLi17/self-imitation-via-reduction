e1d62eb846f8e75e62c7311013dba51cd15ca8d1
Yunfei Li <l-yf16@mails.tsinghua.edu.cn>
2019-12-02 20:44:24 +0800

    Try hack augment on env v5

--- exp_fetchpushwallobstacle-v5.sh
+++ exp_fetchpushwallobstacle-v5.sh
@@ -1,9 +1,8 @@
 # fix z
 CUDA_VISIBLE_DEVICES=2 python test_universal.py --env FetchPushWallObstacle-v5 --policy CustomSACPolicy --gamma 0.99 --random_gripper --num_timesteps 1e7 --log_path logs/FetchPushWallObstacle-v5/her_sac
 CUDA_VISIBLE_DEVICES=0 python test_universal.py --env FetchPushWallObstacle-v5 --policy CustomSACPolicy --gamma 0.95 --random_gripper --num_timesteps 1e7 --log_path logs/FetchPushWallObstacle-v5/her_sac_1
-# fix z, augment
-CUDA_VISIBLE_DEVICES=0 python test_universal_augment.py --env FetchPushWallObstacle-v5 --policy CustomSACPolicy --trained_model logs/FetchPushWallObstacle-v5/her_sac/final.zip --random_gripper --num_timesteps 1e7 --augment_when_success --n_subgoal 10 --log_path logs/FetchPushWallObstacle-v5/her_sac_augment_filter
-CUDA_VISIBLE_DEVICES=0 python test_universal_augment.py --env FetchPushWallObstacle-v5 --policy CustomSACPolicy --trained_model logs/FetchPushWallObstacle-v5/her_sac/final.zip --random_gripper --num_timesteps 1e7 --augment_when_success --n_subgoal 10 --random_ratio 0.7 --log_path logs/FetchPushWallObstacle-v5_random0.7/her_sac_augment_filter
+# random 0.7, augment hack
+CUDA_VISIBLE_DEVICES=0 python test_universal_augment.py --env FetchPushWallObstacle-v5 --policy CustomSACPolicy --trained_model logs/FetchPushWallObstacle-v5/her_sac_1/final.zip --random_gripper --num_timesteps 1e7 --augment_when_success --hack_augment_time --n_subgoal 20 --random_ratio 0.7 --log_path logs/FetchPushWallObstacle-v5_random0.7/her_sac_augment_hack
 # fix z, augment no filter
 CUDA_VISIBLE_DEVICES=0 python test_universal_augment.py --env FetchPushWallObstacle-v5 --policy CustomSACPolicy --trained_model logs/FetchPushWallObstacle-v5/her_sac/final.zip --random_gripper --num_timesteps 1e7 --buffer_size 5e6 --n_subgoal 4 --log_path logs/FetchPushWallObstacle-v5/her_sac_augment_nofilter_4
 CUDA_VISIBLE_DEVICES=0 python test_universal_augment.py --env FetchPushWallObstacle-v5 --policy CustomSACPolicy --trained_model logs/FetchPushWallObstacle-v5/her_sac/final.zip --random_gripper --num_timesteps 1e7 --buffer_size 2e6 --n_subgoal 1 --log_path logs/FetchPushWallObstacle-v5/her_sac_augment_nofilter_1
