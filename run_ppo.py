603553ffd362cc3c4164511c236abeb008f0612b
Yunfei Li <l-yf16@mails.tsinghua.edu.cn>
2019-12-17 17:07:15 +0800

    ppo works even with sparse reward

--- run_ppo.py
+++ run_ppo.py
@@ -9,6 +9,7 @@ from push_wall_obstacle import FetchPushWallObstacleEnv_v4
 # from push_box import FetchPushBoxEnv
 import gym
 from utils.wrapper import DoneOnSuccessWrapper
+import numpy as np
 
 import os, time, argparse, imageio
 import matplotlib.pyplot as plt
@@ -22,10 +23,12 @@ def arg_parse():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--env', default='FetchPushWallObstacle-v4')
     parser.add_argument('--seed', type=int, default=42)
-    parser.add_argument('--num_timesteps', type=float, default=1e7)
+    parser.add_argument('--num_timesteps', type=float, default=1e8)
     parser.add_argument('--log_path', default=None, type=str)
     parser.add_argument('--load_path', default=None, type=str)
+    parser.add_argument('--random_ratio', default=1.0, type=float)
     parser.add_argument('--play', action="store_true", default=False)
+    parser.add_argument('--export_gif', action="store_true", default=False)
     args = parser.parse_args()
     return args
 
@@ -59,7 +62,7 @@ def make_env(env_id, seed, rank, log_dir=None, allow_early_resets=True, kwargs=N
     # env.seed(seed + 10000 * rank)
     return env
 
-def main(env_name, seed, num_timesteps, log_path, load_path, play):
+def main(env_name, seed, num_timesteps, log_path, load_path, play, export_gif, random_ratio):
     log_dir = log_path if (log_path is not None) else "/tmp/stable_baselines_" + time.strftime('%Y-%m-%d-%H-%M-%S')
     configure_logger(log_dir) 
     
@@ -72,7 +75,7 @@ def main(env_name, seed, num_timesteps, log_path, load_path, play):
     elif env_name in ENTRY_POINT.keys():
         env_kwargs = dict(random_box=True,
                       heavy_obstacle=True,
-                      random_ratio=1.0,
+                      random_ratio=random_ratio,
                       random_gripper=True,)
     else:
         raise NotImplementedError("%s not implemented" % env_name)
@@ -84,13 +87,13 @@ def main(env_name, seed, num_timesteps, log_path, load_path, play):
         policy_kwargs = dict(layers=[256, 256])
         # policy_kwargs = {}
         # TODO: vectorize env
-        model = PPO2('MlpPolicy', env, verbose=1, n_steps=2048*8, nminibatches=32, lam=0.95, gamma=0.99, noptepochs=10,
+        model = PPO2('MlpPolicy', env, verbose=1, n_steps=2048, nminibatches=32, lam=0.95, gamma=0.99, noptepochs=10,
                      ent_coef=0.01, learning_rate=3e-4, cliprange=0.2, policy_kwargs=policy_kwargs,
                      )
         def callback(_locals, _globals):
             num_update = _locals["update"]
-            if num_update % 100 == 0:
-                model_path = os.path.join(log_dir, 'model_' + str(num_update // 100))
+            if num_update % 10 == 0:
+                model_path = os.path.join(log_dir, 'model_' + str(num_update // 10))
                 model.save(model_path)
                 print('model saved to', model_path)
             return True
@@ -104,25 +107,44 @@ def main(env_name, seed, num_timesteps, log_path, load_path, play):
         obs = env.reset()
         img = env.render(mode='rgb_array')
         episode_reward = 0.0
+        num_episode = 0
+        frame_idx = 0
         images = []
-        for _ in range(200):
+        for i in range(200):
             images.append(img)
             action, _ = model.predict(obs)
             print('action', action)
             obs, reward, done, _ = env.step(action)
             episode_reward += reward
-            ax.cla()
             img = env.render(mode='rgb_array')
+            ax.cla()
             ax.imshow(img)
-            plt.pause(0.1)
+            ax.set_title('episode ' + str(num_episode) + ', frame ' + str(frame_idx) +
+                         ', goal idx ' + str(np.argmax(obs[-2:])))
+            frame_idx += 1
+            if not export_gif:
+                plt.pause(0.1)
+            else:
+                plt.savefig(os.path.join(os.path.dirname(load_path), 'tempimg%d.png' % i))
             if done:
                 # obs = env.reset()
                 print('episode_reward', episode_reward)
                 episode_reward = 0.0
-        imageio.mimsave(env_name + '.gif', images)
+                frame_idx = 0
+                num_episode += 1
+                if num_episode >= 5:
+                    break
+        # imageio.mimsave(env_name + '.gif', images)
+        if export_gif:
+            os.system('ffmpeg -r 5 -start_number 0 -i ' + os.path.dirname(load_path) + '/tempimg%d.png -c:v libx264 -pix_fmt yuv420p ' +
+                      os.path.join(os.path.dirname(load_path), env_name + '.mp4'))
+            for i in range(200):
+                # images.append(plt.imread('tempimg' + str(i) + '.png'))
+                os.remove(os.path.join(os.path.dirname(load_path), 'tempimg' + str(i) + '.png'))
 
 if __name__ == '__main__':
     args = arg_parse()
     print('arg parsed')
     main(env_name=args.env, seed=args.seed, num_timesteps=int(args.num_timesteps), 
-         log_path=args.log_path, load_path=args.load_path, play=args.play)
+         log_path=args.log_path, load_path=args.load_path, play=args.play, export_gif=args.export_gif,
+         random_ratio=args.random_ratio)
