184260fef8b0913f4f0d187db8300fe054e1774d
Yunfei Li <l-yf16@mails.tsinghua.edu.cn>
2020-01-15 20:32:17 +0800

    Minor changes

--- visualize_pickandplace.py
+++ visualize_pickandplace.py
@@ -4,16 +4,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # env = FetchPickAndPlaceBoxEnv(random_ratio=1.0)
-env = FetchOpenCloseBoxEnv(random_ratio=0.0)
+env = FetchOpenCloseBoxEnv(random_ratio=1.0)
 obs = env.reset()
-fig, ax = plt.subplots(1, 1, figsize=(5, 5))
+fig, ax = plt.subplots(1, 1, figsize=(8, 8))
 for i in range(100):
     ax.cla()
     ax.imshow(env.render(mode='rgb_array'))
     plt.pause(0.1)
     action = np.random.randn(env.action_space.shape[0])
-    action[:3] = obs['observation'][3:6] - obs['observation'][0:3]
-    action[:3] = action[:3] / np.linalg.norm(action[:3])
+    # action[:3] = obs['observation'][3:6] - obs['observation'][0:3]
+    # action[:3] = action[:3] / np.linalg.norm(action[:3])
     obs, _, _, _ = env.step(action)
     # print(i, obs['observation'][:9], obs['achieved_goal'], obs['desired_goal'])
     print(i, obs['observation'][:6], obs['achieved_goal'], obs['desired_goal'])
