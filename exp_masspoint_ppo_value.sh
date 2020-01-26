6ef69e462fe0b5c247b9e4f0434273952d7fe960
Yunfei Li <l-yf16@mails.tsinghua.edu.cn>
2020-01-26 20:41:42 +0800

    Update script

--- exp_masspoint_ppo_value.sh
+++ exp_masspoint_ppo_value.sh
@@ -13,8 +13,11 @@ CUDA_VISIBLE_DEVICES=0 python run_ppo_augment.py --env MasspointPushDoubleObstac
 CUDA_VISIBLE_DEVICES=0 python run_ppo_augment.py --env MasspointPushDoubleObstacle-v1 --num_timesteps 6e8 --random_ratio 1.0 --n_subgoal 2 --parallel --aug_clip 0.0 --reuse_times 8 --log_path logs/MasspointPushDoubleObstacle-v1_uniform/ppo_augment_value/0_reuse8
 # Uniform, reward covers all cases
 CUDA_VISIBLE_DEVICES=0 python run_ppo_augment.py --env MasspointPushDoubleObstacle-v1 --num_timesteps 6e8 --random_ratio 1.0 --n_subgoal 2 --parallel --aug_clip 0.0 --reuse_times 8 --log_path logs/MasspointPushDoubleObstacle-v1_uniform/ppo_augment_value/0_reuse8_adapt
-# Uniform, weight advantage
-CUDA_VISIBLE_DEVICES=0 python run_ppo_augment.py --env MasspointPushDoubleObstacle-v1 --num_timesteps 6e8 --random_ratio 1.0 --n_subgoal 2 --parallel --aug_clip 0.0 --reuse_times 8 --log_path logs/MasspointPushDoubleObstacle-v1_uniform/ppo_augment_value/0_reuse8_weight0.2
+# Uniform, weight advantage, have not add self target yet
+CUDA_VISIBLE_DEVICES=0 python run_ppo_augment.py --env MasspointPushDoubleObstacle-v1 --num_timesteps 6e8 --random_ratio 1.0 --n_subgoal 2 --parallel --aug_clip 0.0 --reuse_times 8 --aug_adv_weight 0.2 --log_path logs/MasspointPushDoubleObstacle-v1_uniform/ppo_augment_value/0_reuse8_weight0.2
+CUDA_VISIBLE_DEVICES=0 python run_ppo_augment.py --env MasspointPushDoubleObstacle-v1 --num_timesteps 6e8 --random_ratio 1.0 --n_subgoal 2 --parallel --aug_clip 0.0 --reuse_times 8 --aug_adv_weight 0.1 --log_path logs/MasspointPushDoubleObstacle-v1_uniform/ppo_augment_value/0_reuse8_weight0.1
+# self target augment
+CUDA_VISIBLE_DEVICES=0 python run_ppo_augment.py --env MasspointPushDoubleObstacle-v1 --num_timesteps 6e8 --random_ratio 1.0 --n_subgoal 2 --parallel --aug_clip 0.0 --reuse_times 8 --aug_adv_weight 0.1 --log_path logs/MasspointPushDoubleObstacle-v1_uniform/ppo_augment_value/0_reuse8_weight0.1_selfaug
 # Hard case 30%, reward covers all cases
 CUDA_VISIBLE_DEVICES=0 python run_ppo_augment.py --env MasspointPushDoubleObstacle-v1 --num_timesteps 4e8 --random_ratio 0.7 --n_subgoal 2 --parallel --aug_clip 0.0 --reuse_times 8 --log_path logs/MasspointPushDoubleObstacle-v1/ppo_augment_value/2_reuse8
 # Hard case 30%, reward covers all cases
