c47c5cd02e2e91257d04a886f2f32bbbd37a9b84
Yunfei Li <l-yf16@mails.tsinghua.edu.cn>
2020-04-16 14:48:17 +0800

    Try sil on stack

--- exp_fetchpush_sac.sh
+++ exp_fetchpush_sac.sh
@@ -1,4 +1,4 @@
 CUDA_VISIBLE_DEVICES=0 python run_her.py --env FetchPushWallObstacle-v4 --random_ratio 0.7 --num_timesteps 1e7 --num_workers 32 --log_path logs/FetchPushWallObstacle-v4_heavy_random0.7_fixz/her_sac_32workers/0
 CUDA_VISIBLE_DEVICES=0 python run_her_augment.py --env FetchPushWallObstacle-v4 --random_ratio 0.7 --num_timesteps 1e7 --num_workers 32 --start_augment 0 --imitation_coef 1 --log_path logs/FetchPushWallObstacle-v4_heavy_random0.7_fixz/her_sac_aug_32workers/start0
-CUDA_VISIBLE_DEVICES=0 python run_her.py --env FetchPushWallObstacle-v4 --random_ratio 0.7 --sil --num_timesteps 1e7 --num_workers 32 --log_path logs/FetchPushWallObstacle-v4_heavy_random0.7_fixz/her_sac_sil_32workers/0
+CUDA_VISIBLE_DEVICES=0 python run_her.py --env FetchPushWallObstacle-v4 --random_ratio 0.7 --sil --num_timesteps 1e7 --num_workers 32 --sil_coef 0.1 --log_path logs/FetchPushWallObstacle-v4_heavy_random0.7_fixz/her_sac_sil_32workers/coef0.1
 
--- exp_fetchstack_sac.sh
+++ exp_fetchstack_sac.sh
@@ -3,6 +3,8 @@ CUDA_VISIBLE_DEVICES=0 python run_her_augment.py --env FetchStack-v1 --num_times
 CUDA_VISIBLE_DEVICES=0 python run_her_augment.py --env FetchStack-v1 --num_timesteps 1e7 --curriculum --num_workers 32 --policy AttentionPolicy --reward_type sparse --n_object 3 --start_augment 1.5e7 --imitation_coef 1 --log_path logs/FetchStack-v1_adapt/her_sac_aug_32workers/attention_critic_3obj_imitation1
 CUDA_VISIBLE_DEVICES=1 python run_her.py --env FetchStack-v1 --num_timesteps 1e7 --curriculum --sequential --num_workers 32 --policy AttentionPolicy --reward_type sparse --n_object 6 --log_path logs/FetchStack-v1_adapt/her_sac_32workers/attention_critic_6obj_seq
 CUDA_VISIBLE_DEVICES=0 python run_her_augment.py --env FetchStack-v1 --num_timesteps 1e7 --curriculum --sequential --num_workers 32 --policy AttentionPolicy --reward_type sparse --n_object 6 --start_augment 2.5e7 --imitation_coef 1 --log_path logs/FetchStack-v1_adapt/her_sac_aug_32workers/attention_critic_6obj_seq_imitation1
+# SIL
+CUDA_VISIBLE_DEVICES=1 python run_her.py --env FetchStack-v1 --num_timesteps 1e7 --curriculum --num_workers 32 --policy AttentionPolicy --reward_type sparse --n_object 2 --sil --sil_coef 0.1 --log_path logs/FetchStack-v1_adapt/her_sac_sil_32workers/attention_critic_2obj_0.1
 CUDA_VISIBLE_DEVICES=0 python run_her.py --env FetchReach-v1 --num_timesteps 1e5 --num_workers 16 --policy CustomSACPolicy
 CUDA_VISIBLE_DEVICES=2 python run_her.py --env FetchPush-v1 --num_timesteps 1e7 --num_workers 16 --policy CustomSACPolicy --log_path logs/FetchPush-v1/her_sac_16workers/no_priority
 CUDA_VISIBLE_DEVICES=2 python run_her.py --env FetchPush-v1 --num_timesteps 1e7 --num_workers 16 --policy CustomSACPolicy --priority --log_path logs/FetchPush-v1/her_sac_16workers/priority
