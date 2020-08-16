d3573ea7a3a604af7e7dba4517630e95ac5a960e
Yunfei Li <yunfeili.cloud@gmail.com>
2020-08-16 19:13:13 +0800

    Implement multi-obstacle masspoint

---
+++ assets/masspoint/generate_xml.py
@@ -0,0 +1,87 @@
+template = '''<?xml version="1.0" encoding="utf-8"?>
+<mujoco>
+    <compiler angle="radian" coordinate="local"></compiler>
+    <option timestep="0.005">
+        <flag warmstart="enable"></flag>
+    </option>
+
+    <asset>
+        <material name="floor_mat" specular="0" shininess="0.5" reflectance="0" rgba="0.2 0.2 0.2 1"></material>
+        <material name="table_mat" specular="0" shininess="0.5" reflectance="0" rgba="0.93 0.93 0.93 1"></material>
+        <material name="block_mat" specular="0" shininess="0.5" reflectance="0" rgba="0.1 0.1 0.5 1"></material>
+        <material name="stick_mat" specular="0" shininess="0.5" reflectance="0" rgba="0.5 0.2 0.0 0.5"></material>
+    </asset>
+
+    <worldbody>
+        {scene}
+		<body pos="0 0 0" name="masspoint">
+			<geom name="masspoint" size="0.15" type="sphere" material="stick_mat" mass="1" euler="1.57 0 0" rgba="0.1 1.0 0.1 1.0"></geom>
+			<joint axis="1 0 0" name="masspoint:slidex" type="slide" damping="0.1"></joint>
+			<joint axis="0 1 0" name="masspoint:slidey" type="slide" damping="0.1"></joint>
+			<joint axis="0 0 1" name="masspoint:slidez" type="slide" damping="10000"></joint>
+			<site name="masspoint" pos="0 0 0" size="0.02 0.02 0.02" rgba="1 0 0 1" type="sphere"></site>
+		</body>
+
+		<body name="object0" pos="0 0 0">
+			<!--joint name="object0:joint" type="free" damping="0.01"></joint!-->
+			<joint axis="1 0 0" name="object0:slidex" type="slide" damping="10"></joint>
+			<joint axis="0 1 0" name="object0:slidey" type="slide" damping="10"></joint>
+            <joint axis="0 0 1" name="object0:slidez" type="slide" damping="10000"></joint>
+            <joint axis="0 0 1" name="object0:rz" type="hinge" damping="10"></joint>
+			<geom size="0.15 0.15 0.15" type="box" name="object0" material="block_mat" mass="1" euler="0 0 0"></geom>
+			<!--geom size="0.025 0.025 0.025" type="sphere" condim="6" name="object0" material="block_mat" mass="2"></geom!-->
+			<site name="object0" pos="0 0 0" size="0.02 0.02 0.02" rgba="1 0 0 1" type="sphere"></site>
+		</body>
+
+		{obstacles}
+
+		<light directional="true" ambient="0.2 0.2 0.2" diffuse="0.8 0.8 0.8" specular="0.3 0.3 0.3" castshadow="false" pos="0 0 4" dir="0 0 -1" name="light0"></light>
+	</worldbody>
+
+	<actuator>
+		<motor gear="100" joint="masspoint:slidex"></motor>
+		<motor gear="100" joint="masspoint:slidey"></motor>
+		<!--position gear="10" joint="masspoint:slidex"></position>
+		<position gear="10" joint="masspoint:slidey"></position!-->
+	</actuator>
+</mujoco>
+'''
+
+scene_template = '''
+<geom name="floor0" pos="2.5 2.5 0" size="30 30 1" type="plane" condim="3" material="floor_mat"></geom>
+		<body name="floor0" pos="2.5 2.5 0">
+			<site name="target0" pos="0.7 0.0 0.4" size="0.10 0.10 0.10" rgba="1 0 0 1" type="sphere"></site>
+		</body>
+
+		<geom name="bound0" pos="{bound0_pos}" size="{bound_v_size}" type="box" mass="2000" material="table_mat"></geom>
+		<geom name="bound1" pos="{bound1_pos}" size="{bound_v_size}" type="box" mass="2000" material="table_mat"></geom>
+		<geom name="bound2" pos="{bound2_pos}" size="{bound_h_size}" type="box" mass="2000" material="table_mat"></geom>
+		<geom name="bound3" pos="{bound3_pos}" size="{bound_h_size}" type="box" mass="2000" material="table_mat"></geom>
+		{walls}
+'''
+
+
+def generate_xml(num_obstacles):
+    wall_template = '<geom name="wall{id}" pos="{pos}" size="0.15 1.0 0.25" type="box" condim="3" mass="100" material="table_mat" rgba="0.93 0.93 0.93 0.5"></geom>'
+    walls = []
+    for i in range(num_obstacles):
+        walls.append(wall_template.format(**dict(id=2 * i, pos=str(1.7 * (i + 1)) + ' 1.0 0.25')))
+        walls.append(wall_template.format(**dict(id=2 * i + 1, pos=str(1.7 * (i + 1)) + ' 4.0 0.25')))
+    scene = scene_template.format(**dict(bound0_pos=str(1.7 * (num_obstacles + 1) / 2) + ' -0.2 0.25',
+                                         bound1_pos=str(1.7 * (num_obstacles + 1) / 2) + ' 5.2 0.25',
+                                         bound2_pos='2.5 -0.2 0.25',
+                                         bound3_pos='2.5 ' + str(1.7 * (num_obstacles + 1) + 0.2) + ' 0.25',
+                                         bound_v_size=str(1.7 * (num_obstacles + 1) / 2) + '0.2 0.25',
+                                         bound_h_size='0.2 2.9 0.25',
+                                         walls="\n".join(walls)))
+    obstacle_template = '''<body name="object{id}" pos="0 0 0">
+			<joint axis="1 0 0" name="object{id}:slidex" type="slide" damping="10"></joint>
+			<joint axis="0 1 0" name="object{id}:slidey" type="slide" damping="10"></joint>
+            <joint axis="0 0 1" name="object{id}:slidez" type="slide" damping="10000"></joint>
+            <joint axis="0 0 1" name="object{id}:rz" type="hinge" damping="10"></joint>
+			<geom size="0.15 0.60 0.15" type="box" name="object{id}" material="stick_mat" mass="5" euler="0 0 0"></geom>
+			<site name="object{id}" pos="0 0 0" size="0.02 0.02 0.02" rgba="0.5 0.2 0 0" type="sphere"></site>
+		</body>'''
+    obstacles = [obstacle_template.format(**dict(id=i + 1)) for i in range(num_obstacles)]
+    xml = template.format(**dict(scene=scene, obstacles="\n".join(obstacles)))
+    return xml
--- masspoint_env.py
+++ masspoint_env.py
@@ -6,6 +6,8 @@ import gym.envs.robotics.utils as robot_utils
 from gym.envs.robotics import rotations
 import numpy as np
 from mujoco_py import MujocoException
+import tempfile
+from assets.masspoint.generate_xml import generate_xml
 
 
 MODEL_XML_PATH0 = os.path.join(os.path.dirname(__file__), 'assets', 'masspoint', 'single_obstacle.xml')
@@ -936,3 +938,182 @@ class MasspointSMazeEnv(MasspointPushEnv, utils.EzPickle):
         if abs(pos[0] - self.pos_wall1[0]) < self.size_wall[0] and abs(pos[1] - 3.0) < 2.0:
             return True
         return False
+
+
+class MasspointPushMultiObstacleEnv(MasspointPushEnv, utils.EzPickle):
+    def __init__(self, reward_type='sparse', n_object=3 + 1, random_box=True,
+                 random_ratio=1.0, random_pusher=False):
+        initial_qpos = {
+            'masspoint:slidex': 1.7 * n_object / 2,
+            'masspoint:slidey': 2.5,
+            'masspoint:slidez': 0.15,
+        }
+        for i in range(n_object):
+            initial_qpos['object%d:slidez' % i] = 0.15
+        self.random_box = random_box
+        self.random_ratio = random_ratio
+        self.random_pusher = random_pusher
+        self.n_object = n_object
+        with tempfile.NamedTemporaryFile(mode='wt',
+                                         dir=os.path.join(os.path.dirname(__file__), 'assets', 'fetch'),
+                                         delete=False, suffix=".xml") as fp:
+            fp.write(generate_xml(self.n_object))
+            model_path = fp.name
+        MasspointPushEnv.__init__(
+            self, model_path, n_substeps=10,
+            target_in_the_air=False, target_offset=0.0,
+            obj_range=1.5, target_range=1.5, distance_threshold=0.30,
+            initial_qpos=initial_qpos, reward_type=reward_type, n_object=n_object)
+        utils.EzPickle.__init__(self)
+        self.pos_walls = [self.sim.model.geom_pos[self.sim.model.geom_name2id('wall%d' % (2*i))] for i in range(self.n_object-1)]
+        self.size_wall = self.sim.model.geom_size[self.sim.model.geom_name2id('wall0')]
+        self.size_obstacle = self.sim.model.geom_size[self.sim.model.geom_name2id('object1')]
+        self.size_object = self.sim.model.geom_size[self.sim.model.geom_name2id('object0')]
+        self.obj_range = np.array([1.7 * n_object / 2 - self.size_object[0], 2.5 - self.size_object[1]])
+        self.obstacle_range = np.array([1.7 - self.size_obstacle[0] - self.size_wall[0], 2.5 - self.size_obstacle[1]])
+
+    def _reset_sim(self):
+        # self.sim.set_state(self.initial_state)
+        sim_state = copy.deepcopy(self.initial_state)
+        # TODO: randomize masspoint pos
+        if self.random_pusher:
+            masspoint_jointx_i = self.sim.model.get_joint_qpos_addr('masspoint:slidex')
+            masspoint_jointy_i = self.sim.model.get_joint_qpos_addr('masspoint:slidey')
+            masspoint_pos = self.initial_masspoint_xpos[:2] + self.np_random.uniform(size=2) * self.obj_range
+            while self.inside_wall(masspoint_pos):
+                masspoint_pos = self.initial_masspoint_xpos[:2] + self.np_random.uniform(size=2) * self.obj_range
+            sim_state.qpos[masspoint_jointx_i] = masspoint_pos[0]
+            sim_state.qpos[masspoint_jointy_i] = masspoint_pos[1]
+        else:
+            masspoint_pos = self.initial_masspoint_xpos[:2]
+
+        def config_valid(object_xpos, obstacles_xpos):
+            assert isinstance(obstacles_xpos, list)
+            conditions = [np.linalg.norm(object_xpos - masspoint_pos) >= 0.3,]
+            for wall_pos in self.pos_walls:
+                conditions.append(abs(object_xpos[0] - wall_pos[0]) >= self.size_object[0] + self.size_wall[0])
+                for obstacle_xpos in obstacles_xpos:
+                    conditions.append(abs(obstacle_xpos[0] - wall_pos[0]) >= self.size_obstacle[0] + self.size_wall[0])
+            for obstacle_xpos in obstacles_xpos:
+                conditions.append(abs(object_xpos[0] - obstacle_xpos[0]) >= self.size_object[0] + self.size_obstacle[0]
+                                  or abs(object_xpos[1] - obstacle_xpos[1]) >= self.size_object[1] + self.size_obstacle[1])
+            return np.all(conditions)
+
+        # Randomize start position of object.
+        if self.random_box and self.np_random.uniform() < self.random_ratio:
+            self.sample_hard = False
+            object_xpos = self.initial_masspoint_xpos[:2] + self.np_random.uniform(size=2) * self.obj_range
+            obstacles_xpos = [np.array([1.7*(i+1), 2.5]) + self.np_random.uniform(size=2) * self.obstacle_range for i in range(self.n_object-1)]
+            while not config_valid(object_xpos, obstacles_xpos):
+                object_xpos = self.initial_masspoint_xpos[:2] + self.np_random.uniform(size=2) * self.obj_range
+                obstacles_xpos = [np.array([1.7 * (i + 1), 2.5]) + self.np_random.uniform(size=2) * self.obstacle_range
+                                  for i in range(self.n_object - 1)]
+        else:
+            self.sample_hard = True
+            object_xpos = self.initial_masspoint_xpos[:2] + self.np_random.uniform(size=2) * self.obj_range
+            obstacles_xpos = [np.array([1.7 * (i + 1) + self.np_random.choice([-1, 1]) * (self.size_wall[0] + self.size_obstacle[0]), 2.5])
+                              for i in range(self.n_object - 1)]
+        # Set the position of box. (two slide joints)
+        box_jointx_i = self.sim.model.get_joint_qpos_addr("object0:slidex")
+        box_jointy_i = self.sim.model.get_joint_qpos_addr("object0:slidey")
+        sim_state.qpos[box_jointx_i] = object_xpos[0]
+        sim_state.qpos[box_jointy_i] = object_xpos[1]
+        for i in range(1, self.n_object):
+            obstacle_jointx_i = self.sim.model.get_joint_qpos_addr("object%d:slidex" % i)
+            obstacle_jointy_i = self.sim.model.get_joint_qpos_addr("object%d:slidey" % i)
+            sim_state.qpos[obstacle_jointx_i] = obstacles_xpos[i - 1][0]
+            sim_state.qpos[obstacle_jointy_i] = obstacles_xpos[i - 1][1]
+        self.sim.set_state(sim_state)
+        self.sim.forward()
+        return True
+
+    def _sample_goal(self):
+        if not hasattr(self, 'size_wall'):
+            self.size_wall = self.sim.model.geom_size[self.sim.model.geom_name2id('wall0')]
+        if not hasattr(self, 'size_object'):
+            self.size_object = self.sim.model.geom_size[self.sim.model.geom_name2id('object0')]
+        if not hasattr(self, 'pos_wall0'):
+            self.pos_wall0 = self.sim.model.geom_pos[self.sim.model.geom_name2id('wall0')]
+        if not hasattr(self, 'pos_wall2'):
+            self.pos_wall2 = self.sim.model.geom_pos[self.sim.model.geom_name2id('wall2')]
+        g_idx = np.random.randint(self.n_object)
+        one_hot = np.zeros(self.n_object)
+        one_hot[g_idx] = 1
+        goal = self.initial_masspoint_xpos[:2] + self.target_offset + self.np_random.uniform(size=2) * self.obj_range
+
+        def same_side(pos0, pos1, sep):
+            if (pos0 - sep) * (pos1 - sep) > 0:
+                return True
+            return False
+
+        if hasattr(self, 'sample_hard') and self.sample_hard:
+            g_idx = 0
+            # if self.np_random.uniform() < 0.6:
+            #     g_idx = 0
+            # else:
+            #     g_idx = np.random.randint(1, self.n_object)
+            one_hot = np.zeros(self.n_object)
+            one_hot[g_idx] = 1
+            object_pos = self.sim.data.get_site_xpos('object0')
+            if g_idx == 0:
+                while np.all([same_side(goal[0], object_pos[0], pos_wall[0]) for pos_wall in
+                              self.pos_walls]) or self.inside_wall(goal):
+                    goal = self.initial_masspoint_xpos[:2] + self.target_offset + self.np_random.uniform(
+                        size=2) * self.obj_range
+        else:
+            while self.inside_wall(goal):
+                goal = self.initial_masspoint_xpos[:2] + self.target_offset + self.np_random.uniform(-self.target_range, self.target_range, size=2)
+        goal = np.concatenate([goal, self.sim.data.get_site_xpos('object' + str(g_idx))[2:3], one_hot])
+        if self.target_in_the_air and self.np_random.uniform() < 0.5:
+            goal[2] += self.np_random.uniform(0, 0.45)
+        return goal.copy()
+
+    def compute_reward(self, observation, goal, info):
+        r, _ = self.compute_reward_and_success(observation, goal, info)
+        return r
+
+    def compute_reward_and_success(self, observation, goal, info):
+        one_hot = goal[3:]
+        idx = np.argmax(one_hot)
+        achieved_goal = observation[3 + 3 * idx: 3 + 3 * (idx + 1)]
+        success = np.linalg.norm(achieved_goal - goal[0:3]) < self.distance_threshold
+        if self.reward_type == "dense":
+            r = 0.1 * MasspointPushEnv.compute_reward(self, achieved_goal, goal[0:3], info) + success
+        else:
+            r = MasspointPushEnv.compute_reward(self, achieved_goal, goal[0:3], info)
+        return r, success
+
+    def step(self, action):
+        try:
+            action = np.clip(action, self.action_space.low, self.action_space.high)
+            self._set_action(action * 20)
+            self.sim.step()
+            self._step_callback()
+            obs = self._get_obs()
+
+            done = False
+            info = {
+                'is_success': self._is_success(obs['achieved_goal'][0:3], self.goal[0:3]),
+            }
+            reward = self.compute_reward(obs['observation'], self.goal, info)
+        except MujocoException:
+            obs = self.reset()
+            done = True
+            info = {'is_success': False}
+            reward = -1
+            print('catch mujoco error, reset env')
+        return obs, reward, done, info
+
+    def _viewer_setup(self):
+        # body_id = self.sim.model.body_name2id('robot0:gripper_link')
+        # lookat = self.sim.data.body_xpos[body_id]
+        lookat = [2.5, 2.5, 0.0]
+        for idx, value in enumerate(lookat):
+            self.viewer.cam.lookat[idx] = value
+        self.viewer.cam.distance = 10.0
+        self.viewer.cam.azimuth = 132.
+        self.viewer.cam.elevation = -60.
+
+    def inside_wall(self, pos):
+        return np.any([abs(pos[0] - pos_wall[0]) < self.size_wall[0]
+                       and abs(pos[1] - 2.5) > 0.5] for pos_wall in self.pos_walls)
